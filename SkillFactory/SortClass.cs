1f7d083054e34d875a676f56c7240caf5c83dd37
Алексей Степанов <sayu94@yandex.ru>
2022-10-19 13:32:02 +0400

    Создал метод эхо

---
+++ SkillFactory/SortClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class SortClass
+{
+	public static void SortClassMetod()
+	{
+
+		int[] array = new int[] {3,5,2,7};
+		int[] sorteddesc;
+		int[] sortedasc;
+		SortArray(in array, out sorteddesc, out sortedasc);
+	}
+	static void SortArray(in int[] array, out int[] sorteddesc, out int[] sortedasc)
+    {
+		sorteddesc = SortArrayDesc( array);
+		sortedasc = SortArrayAsc( array);
+	}
+	static int[] SortArrayDesc(int[]  array)
+    {
+		int[] sorteddesc = array.ToArray();
+		for (int i = 0; i < sorteddesc.Length; i++)
+        {
+			for (int j = i+1; j < sorteddesc.Length; j++)
+            {
+				if(sorteddesc[i] > sorteddesc[j])
+                {
+					int temp = sorteddesc[i];
+					sorteddesc[i] = sorteddesc[j];
+					sorteddesc[j] = temp;
+                }
+            }
+        }
+		return sorteddesc;
+	}
+	static int[] SortArrayAsc( int[] array)
+	{
+		int[] sorteddesc = array.ToArray();
+		for (int i = 0; i < sorteddesc.Length; i++)
+		{
+			for (int j = i + 1; j < sorteddesc.Length; j++)
+			{
+				if (sorteddesc[i] < sorteddesc[j])
+				{
+					int temp = sorteddesc[i];
+					sorteddesc[i] = sorteddesc[j];
+					sorteddesc[j] = temp;
+				}
+			}
+		}
+		return sorteddesc;
+	}
+}
